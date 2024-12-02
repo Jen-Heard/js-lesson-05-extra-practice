705d8b118d24aeb92cb2a053fdde12123cd51061
Jen-Heard <jlheard11@gmail.com>
2024-12-02 15:04:08 -0600

    Completed second exercise

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ fish.addEventListener("mouseover", function() {
 pets.addEventListener("mouseover", function() {
     pets.innerText = "20K";
 });
+
+var colorMode = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var name = document.querySelector("h1");
+var body = document.querySelector("body");
+
+colorMode.addEventListener("click", function(){
+    if (body.classList.contains("light")){
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        h1.innerText = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        h1.innerText = " Party Quincy";
+    }
+});
